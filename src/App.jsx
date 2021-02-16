10ca49806af5398611f691b9525a8e33c1d42720
Huracan <huracan22505@protonmail.com>
2021-02-16 23:00:25 +0200

    feat: complete practice for hw-01

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import PaintingList from './components/PaintingList';
+import Section from './components/Section';
+import paintings from './paintings.json';
+
+export default function App() {
+  return (
+    <div>
+      <Section title="Top of week">
+        <PaintingList items={paintings} />
+      </Section>
+
+      <Section title="best">
+        <PaintingList items={paintings} />
+      </Section>
+    </div>
+  );
+}
