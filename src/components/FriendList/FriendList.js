f5566c58d3b0e0f60aae6ca2753c7c25c74be90e
Huracan <huracan22505@protonmail.com>
2021-02-25 22:44:20 +0200

    refactor: add mentor's advice to task solution

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,21 +1,20 @@
 import PropTypes from 'prop-types';
 import s from './FriendList.module.css';
+import FriendListItem from './FriendListItem';
 
 const FriendList = function ({ friends }) {
   return (
     <ul className={s.friendList}>
-      {friends.map(friend => (
-        <li key={friend.id} className={s.item}>
-          <span className={friend.isOnline ? s.statusOnline : s.offline}></span>
-          <img
-            className={s.avatar}
-            src={friend.avatar}
-            alt={friend.name}
-            width="48"
+      {friends.map(friend => {
+        return (
+          <FriendListItem
+            key={friend.id}
+            name={friend.name}
+            avatar={friend.avatar}
+            isOnline={friend.isOnline}
           />
-          <p className={s.name}>{friend.name}</p>
-        </li>
-      ))}
+        );
+      })}
     </ul>
   );
 };
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import s from './Profile.module.css';
 
-const Profile = ({ avatar, name, tag, location, followers, views, likes }) => {
+const Profile = ({ avatar, name, tag, location, stats }) => {
   return (
     <div className={s.profile}>
       <div className={s.description}>
@@ -14,15 +14,15 @@ const Profile = ({ avatar, name, tag, location, followers, views, likes }) => {
       <ul className={s.stats}>
         <li className={s.item}>
           <span className={s.label}>Followers</span>
-          <span className={s.quantity}>{followers}</span>
+          <span className={s.quantity}>{stats.followers}</span>
         </li>
         <li className={s.item}>
           <span className={s.label}>Views</span>
-          <span className={s.quantity}>{views}</span>
+          <span className={s.quantity}>{stats.views}</span>
         </li>
         <li className={s.item}>
           <span className={s.label}>Likes</span>
-          <span className={s.quantity}>{likes}</span>
+          <span className={s.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
@@ -34,9 +34,11 @@ Profile.propTypes = {
   name: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  followers: PropTypes.number.isRequired,
-  views: PropTypes.number.isRequired,
-  likes: PropTypes.number.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
 };
 
 export default Profile;
