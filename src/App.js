97c8e2277ff364f5911e78c7bcc6d26ca5d8b373
Huracan <huracan22505@protonmail.com>
2021-02-27 16:21:14 +0200

    refactor: add correct import queue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import user from 'db/user.json';
-import statisticalData from 'db/statistical-data.json';
-import friends from 'db/friends.json';
-import transactions from 'db/transactions.json';
-
 import Profile from 'components/Profile';
 import Statistics from 'components/Statistics';
 import FriendList from 'components/FriendList';
 import TransactionHistory from 'components/TransactionHistory';
+
+import user from 'db/user.json';
+import statisticalData from 'db/statistical-data.json';
+import friends from 'db/friends.json';
+import transactions from 'db/transactions.json';
 
 export default function App() {
   return (
